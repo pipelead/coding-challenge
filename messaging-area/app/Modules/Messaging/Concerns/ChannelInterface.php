ed60c8c4ff56a6731c852756cf78924f8ced6f42
Thales Machado <thalesmmachado@gmail.com>
2025-08-28 02:35:56 -0300

    feat: remove get label and get color from ChannelInterface

--- messaging-area/app/Modules/Messaging/Concerns/ChannelInterface.php
+++ messaging-area/app/Modules/Messaging/Concerns/ChannelInterface.php
@@ -7,8 +7,6 @@ use App\Modules\Messaging\Models\Message;
 interface ChannelInterface
 {
     public function getName(): string;
-    public function getLabel(): string;
-    public function getColor(): string;
     public function getSimulationDelay(): int;
     public function supportsReadReceipts(): bool;
     public function sendMessage(Message $message): void;
